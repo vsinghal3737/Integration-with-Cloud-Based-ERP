9c495f53e0528b445b9f67a0b9ef7d20da610bb0
Vaibhav Singhal <vsinghal3737@gmail.com>
2024-05-30 00:04:29 -0600

    placeholder for
    CLIENT_ID = "CLIENT_ID"
    CLIENT_SECRET = "CLIENT_SECRET"

--- src/config.py
+++ src/config.py
@@ -1,3 +1,5 @@
+CLIENT_ID = "CLIENT_ID"
+CLIENT_SECRET = "CLIENT_SECRET"
 REDIRECT_URI = "http://localhost:5000/redirect"
 ENV_SAND = "sandbox"
 BASE_URL = 'https://sandbox-quickbooks.api.intuit.com'
